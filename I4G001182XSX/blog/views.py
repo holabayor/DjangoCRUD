50ee5c30b3940bf2ed3af9a29a83115df1180a23
holabayor <liasu.olabayo@gmail.com>
2022-06-26 23:31:49 +0100

    fixed views and models

--- I4G001182XSX/blog/views.py
+++ I4G001182XSX/blog/views.py
@@ -1,3 +1,31 @@
+from dataclasses import fields
+import imp
+from msilib.schema import ListView
+from pyexpat import model
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
